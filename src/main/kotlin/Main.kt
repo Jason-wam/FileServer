865a76e5afb11aa130bd4d882db99f184d62fbd9
Jason <Jason_wam@163.com>
2022-10-22 22:08:10 +0800

    支持CMD输入参数

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,8 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.netty.handler.codec.http.HttpResponseStatus
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jason.server.respondJson
 import org.jason.server.utils.MediaType
 import org.jason.server.utils.NetworkUtil
@@ -20,42 +22,48 @@ import org.slf4j.LoggerFactory
 import java.io.File
 import java.net.URLEncoder
 import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.concurrent.thread
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val runDir = System.getProperty("user.dir")
-    val propFile = File(runDir, "config.properties")
-    if (propFile.exists().not()) {
-        ClassLoader.getSystemResourceAsStream("config.properties")?.use {
-            propFile.outputStream().use { out ->
-                it.copyTo(out)
+    val files = ArrayList<File>()
+    val properties = Properties()
+    println("args: ${args.joinToString(" | ")}")
+
+    if (args.isNotEmpty()) {
+        //java -jar FileServer.jar -server.port=8080 -server.path=D:/,E:/ -server.pin=123 -ffmpeg=C:/Users/Administrator/Desktop/FileServer/FFmpeg/bin/ffmpeg.exe
+        args.forEach {
+            if (it.startsWith("-")) {
+                val arg = it.removePrefix("-").split("=")
+                if (arg.size == 2) {
+                    val name = arg[0].trim()
+                    val value = arg[1].trim()
+                    properties.setProperty(name, value)
+                }
+            }
+        }
+    } else {
+        File(System.getProperty("user.dir"), "config.properties").also { config ->
+            if (config.exists()) {
+                config.inputStream().use {
+                    properties.load(it)
+                }
+            } else {
+                ClassLoader.getSystemResourceAsStream("config.properties")?.use {
+                    config.outputStream().use { out ->
+                        it.copyTo(out)
+                        exitProcess(0)
+                    }
+                }
             }
         }
     }
 
-    val properties = Properties().apply {
-        propFile.inputStream().use {
-            load(it)
-        }
-    }
-
-    val ipv4 = NetworkUtil.getLocalIPAddress()
-    val pin = properties.getProperty("server.pin", "")
-    val port = properties.getProperty("server.port", "8080").toInt()
-    LoggerFactory.getLogger("Server").info("port: $port ..")
-
-    val ffmpeg = properties.getProperty("ffmpeg", "")
-    if (ffmpeg.isNotBlank()) {
-        LoggerFactory.getLogger("Server").info("ffmpeg: $ffmpeg")
-    }
-
-    val list = ArrayList<File>()
     properties.getProperty("server.path").split(",").forEach {
         if (it.isNotBlank()) {
             val file = File(it)
             if (file.exists()) {
-                list.add(File(it))
+                files.add(File(it))
                 LoggerFactory.getLogger("Server").info("file: $it")
             } else {
                 LoggerFactory.getLogger("Server").error("file: $it not found!")
@@ -63,38 +71,16 @@ fun main(args: Array<String>) {
         }
     }
 
+    val ffmpeg = properties.getProperty("ffmpeg", "")
     if (ffmpeg.isNotBlank()) {
-        thread {
-            fun File.getAllFiles(): List<File> {
-                return ArrayList<File>().apply {
-                    listFiles()?.forEach {
-                        if (it.isDirectory) {
-                            addAll(it.getAllFiles())
-                        } else {
-                            if (MediaType.isVideo(it)) {
-                                add(it)
-                            }
-                        }
-                    }
-                }
-            }
-
-            ThreadPool.instance("thumbnail").setThreads(5)
-            list.forEach {
-                if (it.isDirectory) {
-                    it.getAllFiles().forEach { file ->
-                        ThreadPool.instance("thumbnail").addTask {
-                            createThumbnail(ffmpeg, file)
-                        }
-                    }
-                } else {
-                    createThumbnail(ffmpeg, it)
-                }
-            }
-        }
+        preloadThumbnail(ffmpeg, files)
     }
 
-    LoggerFactory.getLogger("Server").info("Server address: http://$ipv4:$port/")
+    val ip = NetworkUtil.getLocalIPAddress()
+    val pin = properties.getProperty("server.pin", "")
+    val port = properties.getProperty("server.port", "8080").toInt()
+
+    LoggerFactory.getLogger("Server").info("listener: http://$ip:$port/")
 
     embeddedServer(Netty, port, "0.0.0.0") {
         install(PartialContent)
@@ -123,7 +109,7 @@ fun main(args: Array<String>) {
                     put("code", HttpResponseStatus.OK.code())
                     put("path", "/")
                     put("list", JSONArray().apply {
-                        list.forEach { file ->
+                        files.forEach { file ->
                             put(JSONObject().apply {
                                 put("name", file.absolutePath)
                                 put("length", file.length())
@@ -132,12 +118,12 @@ fun main(args: Array<String>) {
                                 put("absolutePath", file.absolutePath)
                                 put("files", file.listFiles()?.size ?: 0)
                                 if (file.isDirectory) {
-                                    put("url", "http://$ipv4:$port/children?path=" + URLEncoder.encode(file.absolutePath, "utf-8"))
+                                    put("url", "http://$ip:$port/children?path=" + URLEncoder.encode(file.absolutePath, "utf-8"))
                                 } else {
-                                    put("url", "http://$ipv4:$port/file?path=" + URLEncoder.encode(file.absolutePath, "utf-8"))
+                                    put("url", "http://$ip:$port/file?path=" + URLEncoder.encode(file.absolutePath, "utf-8"))
                                     if (MediaType.isVideo(file)) {
                                         createThumbnail(ffmpeg, file)?.also {
-                                            put("thumbnail", "http://$ipv4:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
+                                            put("thumbnail", "http://$ip:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
                                         }
                                     }
                                 }
@@ -212,13 +198,13 @@ fun main(args: Array<String>) {
                                 put("absolutePath", it.absolutePath)
                                 put("files", it.listFiles()?.size ?: 0)
                                 if (it.isDirectory) {
-                                    put("url", "http://$ipv4:$port/children?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
+                                    put("url", "http://$ip:$port/children?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
                                 } else {
-                                    put("url", "http://$ipv4:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
+                                    put("url", "http://$ip:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
                                     if (MediaType.isVideo(it)) {
-                                        put("thumbnail", "http://$ipv4:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
+                                        put("thumbnail", "http://$ip:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
                                         createThumbnail(ffmpeg, it)?.also {
-                                            put("thumbnail", "http://$ipv4:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
+                                            put("thumbnail", "http://$ip:$port/file?path=" + URLEncoder.encode(it.absolutePath, "utf-8"))
                                         }
                                     }
                                 }
@@ -259,7 +245,11 @@ fun main(args: Array<String>) {
                     call.respond(HttpStatusCode.NotFound, "File not found!")
                     return@get
                 }
-                call.response.header("Content-Disposition", "inline;filename=${URLEncoder.encode(file.name, "utf-8")}")
+
+                val fileName = withContext(Dispatchers.IO) {
+                    URLEncoder.encode(file.name, "utf-8")
+                }
+                call.response.header("Content-Disposition", "inline;filename=$fileName")
                 call.respondFile(file)
             }
 
@@ -320,6 +310,48 @@ fun main(args: Array<String>) {
             }
         }
     }.start(true)
+}
+
+private fun preloadThumbnail(ffmpeg: String, files: List<File>) {
+    thread {
+        if (files.isNotEmpty()) {
+            LoggerFactory.getLogger("FFmpeg").info("start preload thumbnail..")
+            if (ffmpeg.isNotBlank()) {
+                fun File.getAllFiles(): List<File> {
+                    return ArrayList<File>().apply {
+                        listFiles()?.forEach {
+                            if (it.isDirectory) {
+                                addAll(it.getAllFiles())
+                            } else {
+                                if (MediaType.isVideo(it)) {
+                                    add(it)
+                                }
+                            }
+                        }
+                    }
+                }
+
+                ThreadPool.instance("thumbnail").setThreads(5)
+                ThreadPool.instance("thumbnail").onFinished {
+                    LoggerFactory.getLogger("FFmpeg").info("thumbnail load finished.")
+                }
+
+                files.forEach {
+                    if (it.isDirectory) {
+                        it.getAllFiles().forEach { file ->
+                            ThreadPool.instance("thumbnail").addTask {
+                                createThumbnail(ffmpeg, file)
+                            }
+                        }
+                    } else {
+                        ThreadPool.instance("thumbnail").addTask {
+                            createThumbnail(ffmpeg, it)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 private fun createThumbnail(ffmpeg: String, file: File): File? {
--- src/main/kotlin/org/jason/server/Respond.kt
+++ src/main/kotlin/org/jason/server/Respond.kt
@@ -3,9 +3,7 @@ package org.jason.server
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.util.pipeline.*
 import org.json.JSONObject
-import java.io.File
 
 suspend fun ApplicationCall.respondJson(block: (JSONObject.() -> Unit)? = null) {
     val obj = JSONObject()
